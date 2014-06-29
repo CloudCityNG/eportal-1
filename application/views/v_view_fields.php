3445fdd12c27fec15376fe94348d48201c109276
hansilaF <it12046980@my.sliit.lk>
2014-06-30 02:16:56 +0530

    Removing back button

--- application/views/v_view_fields.php
+++ application/views/v_view_fields.php
@@ -1,3 +1,20 @@
+<link href="<?php echo base_url();?>css/dashboard.css" rel="stylesheet">
+<div class="col-sm-3 col-md-2 sidebar  navbar-default">
+           <div class="h5 text-center dashboard"><a href="<?php echo base_url().'administration'?>"><span class="glyphicon glyphicon-link"></span>&nbsp;Dashboard</a></div>
+          <ul class="nav nav-sidebar">
+            <li class="sub-link dashLink"><a href="<?php echo base_url().'administration/configDetails'?>">Configure Details</a></li>
+            <li class="sub-link dashLink"><a href="<?php echo base_url().'rules/new_ads'?>">Accept Advertisements</a></li>
+			<li class="sub-link dashLink"><a href="<?php echo base_url().'administration/acceptExtend/view/all'?>">Extend Requests</a></li>
+			<li class="sub-link dashLink"><a href="<?php echo base_url().'administration/acceptFeatured/view/all'?>">Featured Requests</a></li>
+			<li class="dashLink active"><a href="<?php echo base_url().'administration/user_management'?>">User Management</a></li>
+			<li class="dashLink"><a href="<?php echo base_url().'report'?>">Generate Reports</a></li>
+			
+          </ul>
+
+          <ul class="nav nav-sidebar ">
+          </ul>
+</div>
+
 <div class="container">
 	<div class="col-md-7 col-md-offset-2">
 		
@@ -316,11 +333,7 @@
   		
   		<div class="form-group">
     		<div class="col-sm-offset-6 col-sm-5">
-      			<?php
-      				$backbtnattributes = array('class' => 'btn btn-default','name'=>'back','value'=>'Back','type'=>'submit', 'content'=>'Back');
-      				echo form_button($backbtnattributes);
-      			?>
-      			&nbsp;
+      			
       			<?php
       				$acceptbtnattributes = array('class' => 'btn btn-primary','name'=>'advertisment_accept','value'=>'Accept');
 					echo form_submit($acceptbtnattributes);
@@ -378,11 +391,7 @@
 	</div>
 	<div class="form-group">
 		<div class="col-sm-offset-6 col-sm-5">
-      			<?php
-      				$backbtnattributes = array('class' => 'btn btn-default','name'=>'back','value'=>'Back','type'=>'submit', 'content'=>'Back');
-      				echo form_button($backbtnattributes);
-      			?>
-      			&nbsp;
+      			
       			<?php
       				$acceptbtnattributes = array('class' => 'btn btn-primary','name'=>'advertisment_accept','value'=>'Accept');
 					echo form_submit($acceptbtnattributes);
